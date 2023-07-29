18ca68df3e1e924de2005010da3aee742516c4da
Kevin <lingyuxiuzhong0208@gmail.com>
2023-07-29 15:27:17 +0800

    proj0 RandomChooser finished

--- proj0/src/aoa/choosers/RandomChooser.java
+++ proj0/src/aoa/choosers/RandomChooser.java
@@ -3,30 +3,48 @@ package aoa.choosers;
 import edu.princeton.cs.algs4.StdRandom;
 import aoa.utils.FileUtils;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RandomChooser implements Chooser {
     private final String chosenWord;
     private String pattern;
 
     public RandomChooser(int wordLength, String dictionaryFile) {
-        // TODO: Fill in/change this constructor.
-        chosenWord = "";
+        if(wordLength < 1){
+            throw new IllegalArgumentException();
+        }
+        List<String> words = FileUtils.readWordsOfLength(dictionaryFile,wordLength);
+        if(words.size() == 0){
+            throw new IllegalStateException();
+        }
+        int numWords = words.size();
+        int randomlyChosenWordNumber = StdRandom.uniform(numWords);
+        chosenWord = words.get(randomlyChosenWordNumber);
+        String str = "-";
+        pattern = str.repeat(chosenWord.length());
     }
 
     @Override
     public int makeGuess(char letter) {
-        // TODO: Fill in this method.
-        return 0;
+        int count = 0;
+        for(int i = 0;i < chosenWord.length();i++){
+            if(chosenWord.charAt(i) == letter){
+                count++;
+                pattern = pattern.substring(0,i) + letter + pattern.substring(i+1);
+            }
+        }
+        return count;
     }
 
     @Override
     public String getPattern() {
-        // TODO: Fill in this method.
-        return "";
+        return pattern;
     }
 
     @Override
     public String getWord() {
-        // TODO: Fill in this method.
-        return "";
+        return chosenWord;
     }
 }
